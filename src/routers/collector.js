9aca7233b12525f841cc8fdbe5c4ca4fdfcdeb0c
gausszhou <gausszhou@qq.com>
2024-12-03 23:37:59 +0800

    perf: 优化写入性能

--- src/routers/collector.js
+++ src/routers/collector.js
@@ -18,20 +18,31 @@ const eventQueue = [];
 const BATCH_SIZE = 200;
 // 定义每轮最多批次
 const BATCH_COUNT = 50;
-// 每 5 秒清理一轮日志
-const WRITE_INTERVAL = 5 * 1000;
+// 定义队列最大长度
+const QUEUE_MAX_LENGTH = BATCH_COUNT * BATCH_SIZE * 2;
+// 每间隔 100 毫秒清理一轮日志
+const WRITE_INTERVAL = 100;
+
+const addEventToQueue = (event) => {
+  // 如果接近队列上限，裁切一半数据
+  if (eventQueue.length > QUEUE_MAX_LENGTH) {
+    eventQueue.slice(0, QUEUE_MAX_LENGTH / 2);
+  }
+  eventQueue.push(event);
+}
 
 // 定时处理队列的函数
 const processEventQueue = async () => {
   // 如果队列为空，直接返回
   if (eventQueue.length === 0) {
-    return;
+    // 执行下一轮检查
+    setTimeout(processEventQueue, WRITE_INTERVAL)
+    return false;
   }
 
   // 提前分好批次
   const batches = [];
-  while (eventQueue.length > 0 && batches.length < BATCH_COUNT) {
-    // 从队列中提取指定数量的数据进行处理
+  while (eventQueue.length > 0) {
     batches.push(eventQueue.splice(0, BATCH_SIZE));
   }
 
@@ -41,17 +52,19 @@ const processEventQueue = async () => {
   for (const batch of batches) {
     index++;
     try {
-      await eventModel.addEvents(batch); // 等待异步操作完成
+      // 等待异步操作完成
+      await eventModel.addEvents(batch); 
       console.log(`${index} / ${batchCount} Processed ${batch.length} events`);
     } catch (error) {
       console.error(`${index} / ${batchCount} Error processing events:`, error);
       // 可以选择在这里重试或记录错误，具体取决于需求
     }
   }
+  // 执行下一轮检查
+  setTimeout(processEventQueue, WRITE_INTERVAL)
 };
 
-// 启动定时器
-setInterval(processEventQueue, WRITE_INTERVAL);
+processEventQueue();
 
 // 接收埋点消息
 router.post("/t", (req, res) => {
@@ -99,8 +112,7 @@ router.post("/t", (req, res) => {
     json = JSON.parse(jsonData);
     const row = createRow(json);
     if (row.event_id) {
-      // eventModel.addEvent(row);
-      eventQueue.push(row);
+      addEventToQueue(row);
     }
   } catch (error) {
     logger.error("Error processing JSON data: ", error);
